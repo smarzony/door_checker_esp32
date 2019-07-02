ab46888eedd301fb7ea2572f97b7c0e8968fb8a1
Piotr Smarzynski <smarzony@gmail.com>
2019-07-02 21:01:39 +0200

    first commit with vs project

---
+++ door_checker_esp32/door_checker_esp32.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		door_checker_esp32.ino
+ Created:	02.07.2019 21:00:32
+ Author:	Piotr
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
